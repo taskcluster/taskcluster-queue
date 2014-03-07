6f4c0acc65a175421027aaae2230de352dedd856
James Lal <james@lightsofapollo.com>
2014-03-07 12:54:40 -0800

    update checks in test.sh

--- test.sh
+++ test.sh
@@ -1,18 +1,14 @@
-#! /bin/bash -e
+#! /bin/bash -vex
 
 # These can safely be run in all cases
 ./node_modules/.bin/nodeunit test/queue/data.js \
   test/events/index.js \
   test/validation/index.js
 
-if [ "$TRAVIS_SECURE_ENV_VARS" == "false" ];
+if [ "$AWS_ACCESS_KEY_ID" == '' ] || [ "$AWS_SECRET_ACCESS_KEY" == '' ];
 then
   echo "Skipping running test that require s3";
 else
-  # aws credentials are required
-  test $AWS_ACCESS_KEY_ID;
-  test $AWS_SECRET_ACCESS_KEY;
-
   echo "Running tests which require s3 credentials"
   ./node_modules/.bin/nodeunit test/api/index.js;
   ./node_modules/.bin/mocha test/api/claim_timeout.js
