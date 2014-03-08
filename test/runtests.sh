e682ce2f7e89211fead95e6bc87b3ab168ff7adf
Jonas Finnemann Jensen <jopsen@gmail.com>
2014-03-08 13:46:26 -0800

    Added DEBUG=* tests

--- test/runtests.sh
+++ test/runtests.sh
@@ -6,12 +6,12 @@
   test/events_test.js
  # test/data_test.js (disabled as it doesn't work reliably)
 
-if [ "$AWS_ACCESS_KEY_ID" == '' ] || [ "$AWS_SECRET_ACCESS_KEY" == '' ];
+if [ "$AWS_ACCESS_KEY_ID" == '' ] ;
 then
   echo "Skipping running test that require s3";
 else
   echo "Running tests which require s3 credentials"
-  ./node_modules/.bin/mocha             \
+  DEBUG=* ./node_modules/.bin/mocha             \
     test/api/claim_timeout.js           \
     test/api/define_schedule_task.js    \
     test/api/post_task.js               \
