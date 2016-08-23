36ae15e0315da6699a493a674da9b6fe04957095
Jonas Finnemann Jensen <jopsen@gmail.com>
2016-08-23 10:36:18 -0700

    More work on workclaimer, tests needed, code pretty much finished

--- src/workclaimer.js
+++ src/workclaimer.js
@@ -14,9 +14,10 @@ let events  = require('events');
  * It's an if, but not an only-if (think over-approximation).
  */
 class HintPoller {
-  constructor(parent, key) {
+  constructor(parent, provisionerId, workerType) {
     this.parent = parent;
-    this.key = key;
+    this.provisionerId = provisionerId;
+    this.workerType = workerType;
     this.requests = [];
     this.started = false;
   }
@@ -24,7 +25,7 @@ class HintPoller {
   requestClaim(count, aborted) {
     // Make a request for count tasks
     let request = null;
-    let result = new Promise(resolve => request = {resolve, count});
+    let result = new Promise(resolve => request = {resolve, reject, count});
     this.requests.push(request);
 
     aborted.then(() => {
@@ -33,44 +34,74 @@ class HintPoller {
       request.resolve([]);
     });
 
-    if (!this.started) {
-      this.start();
-    }
+    this.start();
 
     return result;
   }
 
   start() {
-    this.started = true;
-    this.poll().catch(err => this.parent.emit('error', err));
+    if (!this.started) {
+      this.started = true;
+      this.poll().catch(err => {
+        this.started = false;
+        // Resolve everything as failed
+        this.destroy()
+        let requests = this.requests;
+        this.requests = [];
+        requests.map(r => r.reject(err));
+      }).catch(err => {
+        process.nextTick(() => this.parent.emit('error', err));
+      });
+    }
   }
 
   async poll() {
-    let count = 0;
-    while ((count = _.sumBy(this.requests, 'count')) > 0) {
+    // Get queue objects for pending queues (ordered by priority)
+    let queues = await this.owner.queueService.pendingQueues(
+      this.provisionerId, this.workerType,
+    );
+    // While we have requests for hints
+    while (_.sumBy(this.requests, 'count') > 0) {
+      let claimed = 0; // Count hints claimed
 
-      let queues = this.owner.queueService.pendingQueues(
-        provisionerId, workerType,
-      );
+      // In-order of queues, we claim hints from queues
       for (let queue of queues) {
-        let hints = [];
-        while (count > 0 && (hints = await queue.poll(count)).length > 0) {
-          count -= hints.length;
-          this.claimHint
+        // While limit of hints requested is greater zero, and we are getting
+        // hints from the queue we continue to claim from this queue
+        let limit, hints;
+        let i = 10; // count iterations a limit to 10, before we start over
+        while ((limit = _.sumBy(this.requests, 'count')) > 0 &&
+               (hints = await queue.poll(limit)).length > 0 && i-- > 0) {
+          // Count hints claimed
+          claimed += hints.length;
+
+          // While we have hints and requests for hints we resolve requests
+          while (hints.length > 0 && this.requests.length > 0) {
+            let {resolve, count} = this.requests.shift();
+            resolve(hints.splice(0, count));
+          }
+
+          // Release remaining hints (this shouldn't happen often!)
+          await Promise.all(hints.map(hint => hint.release()));
+          this.parent._monitor.count('hints-released', hints.length);
         }
       }
 
+      // If nothing was claimed, we sleep 200ms before polling again
+      this.parent._monitor.count('hint-poller-claimed', claimed);
+      if (claimed === 0) {
+        await new Promise(resolve => setTimeout(resolve, 200));
+        this.parent._monitor.count('hint-poller-sleep', 1);
+      }
     }
 
+    // No more requests, let's clean-up
     this.destroy();
   }
 
   destroy() {
-    // Remove entry from parent, and clear any timeouts
-    delete this.parent._pending[key];
-    if (this.timeout) {
-      clearTimeout(this.timeout);
-    }
+    // Remove entry from parent
+    delete this.parent._pending[this.provisionerId + '/' + this.workerType];
   }
 }
 
@@ -84,6 +115,7 @@ class WorkClaimer extends events.EventEmitter {
    *   publisher:
    *   Task:
    *   queueService:
+   *   monitor:
    * }
    */
   constructor(options) {
@@ -91,7 +123,9 @@ class WorkClaimer extends events.EventEmitter {
     assert(options.publisher);
     assert(options.Task);
     assert(options.queueService);
+    assert(options.monitor);
     super();
+    this._monitor = monitor;
     this._publisher = options.publisher;
     this._Task = options.Task;
     this._queueService = options.queueService;
@@ -108,19 +142,22 @@ class WorkClaimer extends events.EventEmitter {
     while(claims.length === 0 && !done) {
       // Get a HintPoller
       let key = provisionerId + '/' + workerType;
-      let hintPoller = this._pollers[key];
+      let hintPoller = this._hintPollers[key];
       if (!hintPoller) {
-        this._hintPollers[key] = hintPoller = new HintPoller(this, key);
+        this._hintPollers[key] = hintPoller = new HintPoller(this, provisionerId, workerType);
       }
 
       // Poll for hints (azure messages saying a task may be pending)
-      let hints = await hintPoller.poll(count, aborted);
+      let hints = await hintPoller.requestClaim(count, aborted);
 
+      // Try to claim all the hints
       claims = await Promise.all(hints.map(async(hint) => {
         try {
           return await this.claimTask(hint)
         } catch (err) {
-          this.emit('error', err);
+          this._monitor.reportError(err, 'warning', {
+            comment: 'claimTask from hint failed',
+          });
         }
         return null;
       }));
@@ -133,6 +170,16 @@ class WorkClaimer extends events.EventEmitter {
 
   async claimTask(hint) {
     // See: queue.claimTask in api.js (preserve ordering of operations!)
+
+    // remove hint when we're done
+    hint.remove().catch(err => {
+      this._monitor.reportError(err, 'warning', {
+        comment: 'hint.remove() in claimTask -- error ignored',
+      });
+    });
+
+    // Return null, if we couldn't claim the task
+    return null;
   }
 }
 
