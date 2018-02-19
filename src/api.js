b3a2b2c7bc836f21b8c46d3e449619144d0c77b9
fiennyangeln <fifi.angelina@yahoo.com>
2018-02-19 16:03:03 +0800

    console.log to run test

--- src/api.js
+++ src/api.js
@@ -174,10 +174,10 @@ api.declare({
       taskId: req.params.taskId,
     });
   }
-
+  console.log(task);
   // Create task definition
   let definition = await task.definition();
-
+  console.log(definition);
   return res.reply(definition);
 });
 
